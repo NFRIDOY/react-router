f26fad57786ee2a72876785aa4b720f60167cd5b
NF RIDOY <nfridoy@gmail.com>
2023-09-21 01:10:59 +0600

    bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
-import { Link, Outlet } from 'react-router-dom';
+// import { Link, Outlet } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer'
+import { Outlet } from 'react-router-dom';
 
 function App() {
   // const [count, setCount] = useState(0)
